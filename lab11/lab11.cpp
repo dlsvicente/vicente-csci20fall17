91137f639cbc197ed198d7418a74320dca0d9d47
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-08-25 02:05:29 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,8 @@
+computer generates a random number between 1 and 20.
+the user guesses the number that the computer generates.
+The computer will specify whether the user guessed lower or higher than generated number until user guesses correct number.
+If the user is wrong, then take away 1 point from 10 points. 
+if user guesses generated number correctly add 1 point to 10 points.
+git add lab11
+git commit -a -m "Lab1.1 submission"
+git push
