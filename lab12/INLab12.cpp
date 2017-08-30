dedad987b64bdb1f9d23de5494607a9bb70e070d
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-08-30 01:30:11 +0000

    INLab12.cpp

---
+++ lab12/INLab12.cpp
@@ -0,0 +1,11 @@
+ #include <iostream> 
+ using namespace std;
+ 
+ int main()
+{
+ cout<<" \\ ,_____, / "<<endl;
+ cout<<"  ‘(@ , @)’ "<<endl;
+ cout<<" ((|||||||)) "<<endl;
+ cout<<"   ||   ||   "<<endl;
+ cout<<"   /|\\  /|\\    "<<endl;
+}
---
+++ lab12/lab12.cpp
@@ -0,0 +1,19 @@
+//Created By: April Browne
+//Created On: 8/22/2016
+ 
+ #include <iostream> 
+ using namespace std;
+ 
+ int main()
+ {
+     cout<<"Hello"<<"World!"<<endl;
+     cout<<"hello"<<"Vicente"<<endl; // main function
+ }
+ 
+ //Identify the following parts of the program: // comments
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
+ 
