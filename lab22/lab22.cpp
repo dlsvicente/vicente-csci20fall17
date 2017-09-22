46409efa829827c6e51c14912c33ca1de0124c15
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-09-22 01:48:25 +0000

    Lab2.2 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // this is the input output stream.
+#include <ctime> // this includes the time function so we can use it. 
+using namespace std; // this includes the different function types.
+
+
+  void ranNumGen() // this was the name of my random number generator function name.
+ {
+    
+    srand(time(0)); // this is where I seeded the random number to one. 
+    int randNum = rand() % 100 + 1; // this is my random number generating formula
+    cout << "Your Random Number: " << randNum << endl; // this is the last output in my function that prints out the random number (randNum).
+ }
+ int main() {
+   ranNumGen(); // this is just calling for the function. 
+}
+
+
+
+
