c642e3bb422f5662bdb2a733cb35a5c91fe57473
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-10-13 02:58:49 +0000

    lab32 submission

---
+++ lab32/lab32.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main ()
+{
+    
+    string fir_name1;
+    string fir_name2;
+    string last_name;
+    char mar_stat;
+    int gross_wage;
+    int with_held;
+    double adj_gross_inc;
+    
+    
+   cout << "what is you Marital Status: ";
+   cin >> mar_stat; 
+   
+    
+    if (mar_stat == m ) || (mar_stat == M){
+    
+    adj_gross_inc = (gross_wage - 4050) - 12,700);
+     else if (adj_gross_inc )
+    }
+}
