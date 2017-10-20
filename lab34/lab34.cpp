45844ef88d1d0572934ebec623f51aeb59ec4148
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-10-20 02:43:45 +0000

    Lec 3.5 Submission

--- lab34/lab34.cpp
+++ lab34/lab34.cpp
@@ -33,7 +33,13 @@ while (ch != "-1") {
             
             /* 
             Debugging Method: I read the diresctions that you provided us. I inputted the code into my compiler and ran it. 
-            I studied the output, and the way that it reacted  
+            I studied the output, and the way that it reacted to my input. I realized that the way that the code was written was a little crowded
+            so I decided to push both if conditions that went together into one line and combine them with an "and" statement. 
+            The method that i used to figure out this bug was a trial an error method. I tried to look at each piece of the code line from line,
+            but it did not help me. The debugging method that will work best will always depend on the experience and preference of the coder, and also 
+            on the style and type of code that you are debugging. Some methods may have worked better for someone else that was debugging this code, but trial and error
+            was what i felt help me out the most. 
+            
             
             
             */
