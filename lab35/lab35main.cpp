a715440cd91b2cfd08ca8c298baddbd571149840
Vicente De Los Santos H <vdelossantos002@student.butte.edu>
2017-10-25 02:29:37 +0000

    Lab 3.5 Submission

--- lab35/lab35main.cpp
+++ lab35/lab35main.cpp
@@ -24,3 +24,45 @@ int main ()
     
     return 0;
 }
+
+/*
+
+Temperature in Kelvin: 0
+
+Temperature in Fahrenheit: -273.15
+
+temperature in Celsius: -459.67
+
+Temperature in Kelvin: 274
+
+Temperature in Fahrenheit: 0.85
+
+temperature in Celsius: 33.53
+
+400.15
+Temperature in Kelvin: 400.15
+
+Temperature in Fahrenheit: 127
+
+temperature in Celsius: 260.6
+
+32
+Temperature in Kelvin: 305.15
+
+Temperature in Fahrenheit: 32
+
+temperature in Celsius: 89.6
+
+32
+Temperature in Kelvin: 273.15
+
+Temperature in Fahrenheit: 0
+
+temperature in Celsius: 32
+
+
+
+Process exited with code: 0
+
+
+*/
